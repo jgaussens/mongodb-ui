323282c93fa62c1fe29efb6dcc6fd743e148e243
jgaussens <gaussensjules@gmail.com>
2018-03-14 11:27:17 +0100

    first tries

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const bodyParser= require('body-parser')
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+const MongoClient = require('mongodb').MongoClient
+
+
+/*
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("restaurant_Inspections");
+  dbo.collection('restaurants').find({}).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    db.close();
+  });
+});
+
+*/
+
+var db
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('restaurant_Inspections') // whatever your database name is
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+})
+
+
+/*
+app.listen(3000, function() {
+  console.log('listening on 3000')
+})
+
+app.get('/', (req, res) => {
+  var cursor = db.collection('restaurants').find({})
+})
+*/
+
+
+app.get('/', (req, res) => {
+  var cursor = db.collection('restaurants').find({"cuisine": "Japanese", "borough": "Manhattan"},{"grades":1, "borough":1})
+})	
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+
+
+app.post('/restaurants', (req, res) => {
+  console.log(req.body)
+})
+
+console.log('May Node be with you')
+
+/* Once we’re done saving, we have to redirect the user somewhere (here we are in the current tutorial*/
--- test.js
+++ test.js
@@ -2,13 +2,14 @@ var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("restaurant_Inspections");
-  var query = { address: "Park Lane 38" };
-  dbo.collection('restaurants').find({}).toArray(function(err, result) {
-    if (err) throw err;
-    console.log(result);
-    db.close();
-  });
-});
+function connect(){
+	MongoClient.connect(url, function(err, db) {
+	  if (err) throw err;
+	  var dbo = db.db("restaurant_Inspections");
+	  dbo.collection('restaurants').find({}).toArray(function(err, result) {
+	    if (err) throw err;
+	    console.log(result);
+	    db.close();
+	  });
+	});
+}
